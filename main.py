ea4647cc3bd009ca7c764997daf1a7d6523569ba
Edilson Rodrigues <edilson.w3g@gmail.com>
2022-06-19 18:34:33 -0300

    feat: rate limite on endpoints

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from starlette.requests import Request
 import uvicorn
+
 
 from app.internal import admin
 from app.routers import item
 
-
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(admin.router)
 app.include_router(item.router)
 
 
 @app.get('/ping')
-async def root():
+@limiter.limit("1/second")
+async def root(request: Request):
     return {'message': 'OK'}
 
 if __name__ == '__main__':
