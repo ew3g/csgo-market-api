dfb809719f5d90ca2ead1cfff6c00d43846b2d6a
Edilson Rodrigues <edilson.w3g@gmail.com>
2022-06-23 19:51:25 -0300

    test: get item coverage

--- test/schemas.py
+++ test/schemas.py
@@ -119,3 +119,69 @@ empty_item_list_response_schema = {
     "message"
   ]
 }
+
+item_response_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "data": {
+      "type": "object",
+      "properties": {
+        "id": {
+          "type": "string"
+        },
+        "name": {
+          "type": "string"
+        },
+        "type": {
+          "type": "string"
+        },
+        "subtype": {
+          "type": "string"
+        },
+        "game_type": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "id",
+        "name",
+        "type",
+        "subtype",
+        "game_type"
+      ]
+    },
+    "code": {
+      "type": "integer"
+    },
+    "message": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "data",
+    "code",
+    "message"
+  ]
+}
+
+item_not_found_response_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "error": {
+      "type": "string"
+    },
+    "code": {
+      "type": "integer"
+    },
+    "message": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "error",
+    "code",
+    "message"
+  ]
+}
--- test/test.py
+++ test/test.py
@@ -1,7 +1,13 @@
 from main import app
 from fastapi.testclient import TestClient
 from jsonschema import validate, exceptions
-from schemas import empty_item_list_response_schema, item_list_response_schema, unprocessable_entity_response_schema
+from schemas import (
+    empty_item_list_response_schema,
+    item_response_schema,
+    item_list_response_schema,
+    item_not_found_response_schema,
+    unprocessable_entity_response_schema
+)
 from dotenv import load_dotenv, find_dotenv
 
 import os
@@ -52,3 +58,17 @@ def test_get_item_list_empty_ok():
     assert 200 == response.status_code
     assert validate_json(json_response, empty_item_list_response_schema)
     assert 0 == len(json_response["data"])
+
+
+def test_get_item_ok():
+    response = client.get("/item/629d4b340fb273faf5e846db", headers={"x-token": os.getenv("API_TOKEN")})
+    json_response = response.json()
+    assert 200 == response.status_code
+    assert validate_json(json_response, item_response_schema)
+
+
+def test_get_item_not_found():
+    response = client.get("/item/629d4b340fb273faf5e846d2", headers={"x-token": os.getenv("API_TOKEN")})
+    json_response = response.json()
+    assert 404 == json_response["code"]
+    assert validate_json(json_response, item_not_found_response_schema)
